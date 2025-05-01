7d354e706ef2a467f572eb5b29c55a42fa63a1e4
Tania <tania.flyora@gmail.com>
2025-05-01 14:43:23 +0300

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/hello/')
+      .then(res => setMessage(res.data.message))
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
